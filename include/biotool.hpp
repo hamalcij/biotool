16f46fc8beca395fa6f4914b49bf52dd5c93167d
Hamalčík Jan <jan.hamalcik@icloud.com>
2020-04-18 16:41:53 +0200

    Create include/biotool.hpp

---
+++ include/biotool.hpp
@@ -0,0 +1,15 @@
+// biotool
+// Bioinformatics Toolbox
+//
+// include/biotool.hpp
+// Copyright (c) 2020 Hamalčík Jan
+//
+// API include file, provides access to biotool's features
+//
+
+#ifndef BIOTOOL_HPP
+#define BIOTOOL_HPP
+
+
+
+#endif // BIOTOOL_HPP
