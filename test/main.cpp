2c49674348d974b7652d8ffa213bdf9381ec582d
Hamalčík Jan <jan.hamalcik@icloud.com>
2020-04-29 01:47:54 +0200

    Make tests interactive

--- test/main.cpp
+++ test/main.cpp
@@ -34,17 +34,61 @@ SOFTWARE.
 #include "test.hpp"
 
 int main() {
-  TestFasta fasta("/Users/janhamalcik/github/biotool/test/testfiles/fasta");
-  fasta.performTest();
+  std::cout << "This is biotool's test program." << std::endl;
 
-  TestSequencePair sequencePair("SHORTS", "HORSTS");
-  sequencePair.performTest();
+  for (;;) {
+    int answer;
+    std::string path;
+    std::string seq1;
+    std::string seq2;
 
-  TestPdb pdb("/Users/janhamalcik/github/biotool/test/testfiles/hemoglobin.pdb");
-  pdb.performTest();
+    std::cout << std::endl;
+    std::cout << "########################################################" << std::endl;
+    std::cout << std::endl;
+    std::cout << "To test the FASTA parser, type: 1" << std::endl;
+    std::cout << "To test the sequence pair analysis, type: 2" << std::endl;
+    std::cout << "To test the PDB parser, type: 3" << std::endl;
+    std::cout << "To test the Clustal parser, type: 4" << std::endl;
+    std::cout << "To exit biotool's test program, type: 0" << std::endl;
+    std::cin >> answer;
+    std::cout << std::endl;
 
-  TestClustal clustal("/Users/janhamalcik/github/biotool/test/testfiles/clustal");
-  clustal.performTest();
+    if (answer == 1) {
+      std::cout << "Type full path to your .fasta file:" << std::endl;
+      std::cin >> path;
+      TestFasta fasta(path);
+      fasta.performTest();
+    }
 
-  return 0;
+    else if (answer == 2) {
+      std::cout << "Type first sequence:" << std::endl;
+      std::cin >> seq1;
+      std::cout << "Type second sequence:" << std::endl;
+      std::cin >> seq2;
+      TestSequencePair sequencePair(seq1, seq2);
+      sequencePair.performTest();
+    }
+
+    else if (answer == 3) {
+      std::cout << "Type full path to your .pdb file:" << std::endl;
+      std::cin >> path;
+      TestPdb pdb(path);
+      pdb.performTest();
+    }
+
+    else if (answer == 4) {
+      std::cout << "Type full path to your clustal file:" << std::endl;
+      std::cin >> path;
+      TestClustal clustal(path);
+      clustal.performTest();
+    }
+
+    else if (answer == 0) {
+      return 0;
+    }
+
+    else {
+      throw std::invalid_argument("\"" + std::to_string(answer) + "\" is an invalid argument!");
+    }
+  }
 }
--- test/test.hpp
+++ test/test.hpp
@@ -35,6 +35,8 @@ SOFTWARE.
 #include "../biotool.hpp"
 
 #include<iostream>
+#include<string>
+#include<exception>
 
 using namespace biotool;
 
@@ -43,14 +45,22 @@ public:
   TestFasta(const std::string& file) : myFasta_(FASTA(file)) {}
 
   void performTest() {
-    std::cout << "TEST: Fasta" << std::endl;
+    int from, to;
+
+    std::cout << std::endl;
+    std::cout << "########################################################" << std::endl;
+    std::cout << std::endl;
+    std::cout << "TEST: FASTA" << std::endl;
+    std::cout << "Type two sequence positions separated by space (e.g. 2 24). Subsequence spanning these positions will be fetched for each molecule:" << std::endl;
+    std::cin >> from >> to;
+    std::cout << std::endl;
     for (std::size_t i = 0; i < NUMBER_OF_MOLECULES(myFasta_); ++i) {
       MOLECULE myMolecule(myFasta_, i);
-      std::cout << "NEW MOLECULE:" << std::endl;
+      std::cout << "MOLECULE:" << std::endl;
       std::cout << "Description: " << DESCRIPTION(myMolecule) << std::endl;
       std::cout << "Sequence: " << SEQUENCE(myMolecule) << std::endl;
       std::cout << "Sequence length: "<< SEQUENCE_LENGTH(myMolecule) << std::endl;;
-      std::cout << "Subsequence 2...24: " << SUBSEQUENCE(myMolecule, 2, 24) << std::endl;
+      std::cout << "Subsequence " << from << "..." << to << ": " << SUBSEQUENCE(myMolecule, from, to) << std::endl;
       std::cout << std::endl;
     }
   }
@@ -64,19 +74,30 @@ public:
   TestSequencePair(const std::string& seq1, const std::string& seq2) : mySequencePair_(SEQUENCE_PAIR(seq1, seq2)) {}
 
   void performTest() {
+    std::cout << std::endl;
+    std::cout << "########################################################" << std::endl;
+    std::cout << std::endl;
     std::cout << "TEST: Sequence pair" << std::endl;
+    std::cout << std::endl;
     auto[sequence1, sequence2] = SEQUENCES(mySequencePair_);
     auto[editDistance, alignment] = EDIT_DISTANCE(mySequencePair_);
 
     std::cout << "Sequence 1: " << sequence1 << std::endl;
     std::cout << "Sequence 2: " << sequence2 << std::endl;
-    std::cout << "Hamming distance: " << HAMMING_DISTANCE(mySequencePair_) << std::endl;
+    try {
+      std::cout << "Hamming distance: " << HAMMING_DISTANCE(mySequencePair_) << std::endl;
+    }
+    catch (...) {
+      std::cout << "Cannot be computed, sequences do not have the same length!" << std::endl;
+    }
     std::cout << "Edit distance: " << editDistance << std::endl;
 
+    int i = 1;
     for (auto&& [seq1, seq2] : alignment) {
-      std::cout << std::endl;
-      std::cout << seq1 << std::endl;
-      std::cout << seq2 << std::endl;
+      std::cout << "Optimal alignment " << i << ":" << std::endl;
+      std::cout << "  " << seq1 << std::endl;
+      std::cout << "  " << seq2 << std::endl;
+      ++i;
     }
     std::cout << std::endl;
   }
@@ -90,7 +111,32 @@ public:
   TestPdb(const std::string& path) : myPdb_(PDB(path)) {}
 
   void performTest() {
-    std::cout << "PDB" << std::endl;
+    std::string modelID, residueID, atomID, hetID, hetAtomID;
+    char chainID;
+    float maxDistance;
+
+    std::cout << std::endl;
+    std::cout << "########################################################" << std::endl;
+    std::cout << std::endl;
+    std::cout << "TEST: PDB" << std::endl;
+    std::cout << "Type ID of a model (if the .pdb file doesn't specify a model, type simply \"1\"):" << std::endl;
+    std::cin >> modelID;
+    std::cout << "Type ID of a chain within model " << modelID << ":" << std::endl;
+    std::cin >> chainID;
+    std::cout << "Type ID of a residue within model " << modelID << " and chain " << chainID << ":" << std::endl;
+    std::cin >> residueID;
+    std::cout << "Type ID of an atom within model " << modelID << " and chain " << chainID << " and residue " << residueID << ":" << std::endl;
+    std::cin >> atomID;
+    std::cout << "Type ID of a ligand within model " << modelID << " and chain " << chainID << " (if there is no such ligand, just press enter):" << std::endl;
+    std::cin >> hetID;
+    if (!hetID.empty()) {
+      std::cout << "Type ID of an atom within model " << modelID << " and chain " << chainID << " and ligand " << hetID << ":" << std::endl;
+      std::cin >> hetAtomID;
+      std::cout << "Type a floating point number representing a maximum distance to be searched for atoms and residues from ligand " << hetID << ":" << std::endl;
+      std::cin >> maxDistance;
+    }
+    std::cout << std::endl;
+
     std::cout << "Models: " << NUMBER_OF_MODELS(myPdb_) << std::endl;
 
     for (auto&& model : GET_MODELS(myPdb_)) {
@@ -153,105 +199,147 @@ public:
     }
     std::cout << std::endl;
 
-    MODEL myModel = FIND_MODEL(myPdb_, "1");
-    std::cout << "Found model " << ID(myModel);
-    std::cout << "; Chains: " << NUMBER_OF_CHAINS(myModel);
-    std::cout << "; Residues: " << NUMBER_OF_RESIDUES(myModel);
-    std::cout << "; Atoms: " << NUMBER_OF_ATOMS(myModel);
-    std::cout << "; HetResidues: " << NUMBER_OF_HET_RESIDUES(myModel);
-    std::cout << "; HetAtoms: " << NUMBER_OF_HET_ATOMS(myModel);
-    std::cout << std::endl;
-
-    CHAIN myChain = FIND_CHAIN(myModel, 'B');
-    std::cout << "  Found chain " << ID(myChain);
-    std::cout << "; Residues: " << NUMBER_OF_RESIDUES(myChain);
-    std::cout << "; Atoms: " << NUMBER_OF_ATOMS(myChain);
-    std::cout << "; HetResidues: " << NUMBER_OF_HET_RESIDUES(myChain);
-    std::cout << "; HetAtoms: " << NUMBER_OF_HET_ATOMS(myChain);
-    std::cout << std::endl;
-
-    RESIDUE myResidue = FIND_RESIDUE(myChain, "37");
-    std::cout << "    Found residue " << RESIDUE_NAME(myResidue) << " " << ID(myResidue);
-    std::cout << "; Atoms: " << NUMBER_OF_ATOMS(myResidue);
-    std::cout << std::endl;
-
-    ATOM myAtom = FIND_ATOM(myResidue, "1338");
-    auto[x, y, z] = COORDINATES(myAtom);
-    std::cout << "      Found atom " << ATOM_NAME(myAtom) << " " << ID(myAtom);
-    std::cout << "; (" << x << ", " << y << ", " << z << ")";
-    std::cout << "; Element: " << ELEMENT(myAtom);
-    std::cout << "; Charge: " << CHARGE(myAtom);
-    std::cout << std::endl;
-
-    HET_RESIDUE myHetResidue = FIND_HET_RESIDUE(myChain, "147");
-    std::cout << "    Found HET residue " << RESIDUE_NAME(myHetResidue) << " " << ID(myHetResidue);
-    std::cout << "; Atoms: " << NUMBER_OF_HET_ATOMS(myHetResidue);
-    std::cout << std::endl;
-
-    ATOMS nearAtoms;
-    float maxDistance = 3.2;
-    GET_ATOMS_CLOSE_TO_LIGAND(myModel, myHetResidue, nearAtoms, maxDistance);
-    for (auto&& atom : nearAtoms) {
-      auto[nearX, nearY, nearZ] = COORDINATES(atom);
-      std::cout << "      Near Atom within " << maxDistance << "Å ";
-      std::cout << ATOM_NAME(atom) << " " << ID(atom);
-      std::cout << "; Coords (" << nearX << ", " << nearY << ", " << nearZ << ")";
-      std::cout << std::endl;
-    }
-
-    RESIDUES nearResidues;
-    maxDistance = 2.6;
-    GET_RESIDUES_CLOSE_TO_LIGAND(myModel, myHetResidue, nearResidues, maxDistance);
-    for (auto&& residue : nearResidues) {
-      std::cout << "      Near Residue within " << maxDistance << "Å ";
-      std::cout << RESIDUE_NAME(residue) << " " << ID(residue);
-      std::cout << "; Atoms: " << NUMBER_OF_ATOMS(residue);
+    try {
+      MODEL myModel = FIND_MODEL(myPdb_, modelID);
+      std::cout << "Found model " << ID(myModel);
+      std::cout << "; Chains: " << NUMBER_OF_CHAINS(myModel);
+      std::cout << "; Residues: " << NUMBER_OF_RESIDUES(myModel);
+      std::cout << "; Atoms: " << NUMBER_OF_ATOMS(myModel);
+      std::cout << "; HetResidues: " << NUMBER_OF_HET_RESIDUES(myModel);
+      std::cout << "; HetAtoms: " << NUMBER_OF_HET_ATOMS(myModel);
       std::cout << std::endl;
 
-      ATOMS nearResAtoms;
-      GET_ATOMS(residue, nearResAtoms);
-      for (auto&& atom : nearResAtoms) {
-        auto[aX, aY, aZ] = COORDINATES(atom);
-        std::cout << "        Atom " << ATOM_NAME(atom) << " " << ID(atom);
-        std::cout << "; (" << aX << ", " << aY << ", " << aZ << ")";
-        std::cout << "; Element: " << ELEMENT(atom);
-        std::cout << "; Charge: " << CHARGE(atom);
+      try {
+        CHAIN myChain = FIND_CHAIN(myModel, chainID);
+        std::cout << "  Found chain " << ID(myChain);
+        std::cout << "; Residues: " << NUMBER_OF_RESIDUES(myChain);
+        std::cout << "; Atoms: " << NUMBER_OF_ATOMS(myChain);
+        std::cout << "; HetResidues: " << NUMBER_OF_HET_RESIDUES(myChain);
+        std::cout << "; HetAtoms: " << NUMBER_OF_HET_ATOMS(myChain);
         std::cout << std::endl;
+
+        try {
+          RESIDUE myResidue = FIND_RESIDUE(myChain, residueID);
+          std::cout << "    Found residue " << RESIDUE_NAME(myResidue) << " " << ID(myResidue);
+          std::cout << "; Atoms: " << NUMBER_OF_ATOMS(myResidue);
+          std::cout << std::endl;
+
+          try {
+            ATOM myAtom = FIND_ATOM(myResidue, atomID);
+            auto[x, y, z] = COORDINATES(myAtom);
+            std::cout << "      Found atom " << ATOM_NAME(myAtom) << " " << ID(myAtom);
+            std::cout << "; (" << x << ", " << y << ", " << z << ")";
+            std::cout << "; Element: " << ELEMENT(myAtom);
+            std::cout << "; Charge: " << CHARGE(myAtom);
+
+            std::cout << std::endl;
+
+            if (!hetID.empty()) {
+
+              try {
+                HET_RESIDUE myHetResidue = FIND_HET_RESIDUE(myChain, hetID);
+                std::cout << "    Found HET residue " << RESIDUE_NAME(myHetResidue) << " " << ID(myHetResidue);
+                std::cout << "; Atoms: " << NUMBER_OF_HET_ATOMS(myHetResidue);
+                std::cout << std::endl;
+
+                try {
+                  ATOMS nearAtoms;
+                  GET_ATOMS_CLOSE_TO_LIGAND(myModel, myHetResidue, nearAtoms, maxDistance);
+                  for (auto&& atom : nearAtoms) {
+                    auto[nearX, nearY, nearZ] = COORDINATES(atom);
+                    std::cout << "      Near Atom within " << maxDistance << "Å ";
+                    std::cout << ATOM_NAME(atom) << " " << ID(atom);
+                    std::cout << "; Coords (" << nearX << ", " << nearY << ", " << nearZ << ")";
+                    std::cout << std::endl;
+                  }
+
+                  try {
+                    RESIDUES nearResidues;
+                    GET_RESIDUES_CLOSE_TO_LIGAND(myModel, myHetResidue, nearResidues, maxDistance);
+                    for (auto&& residue : nearResidues) {
+                      std::cout << "      Near Residue within " << maxDistance << "Å ";
+                      std::cout << RESIDUE_NAME(residue) << " " << ID(residue);
+                      std::cout << "; Atoms: " << NUMBER_OF_ATOMS(residue);
+                      std::cout << std::endl;
+
+                      ATOMS nearResAtoms;
+                      GET_ATOMS(residue, nearResAtoms);
+                      for (auto&& atom : nearResAtoms) {
+                        auto[aX, aY, aZ] = COORDINATES(atom);
+                        std::cout << "        Atom " << ATOM_NAME(atom) << " " << ID(atom);
+                        std::cout << "; (" << aX << ", " << aY << ", " << aZ << ")";
+                        std::cout << "; Element: " << ELEMENT(atom);
+                        std::cout << "; Charge: " << CHARGE(atom);
+                        std::cout << std::endl;
+                      }
+                    }
+
+                    try {
+                      HET_ATOM myHetAtom = FIND_ATOM(myHetResidue, hetAtomID);
+                      auto[hetX, hetY, hetZ] = COORDINATES(myHetAtom);
+                      std::cout << "      Found atom " << ATOM_NAME(myHetAtom) << " " << ID(myHetAtom);
+                      std::cout << "; (" << hetX << ", " << hetY << ", " << hetZ << ")";
+                      std::cout << "; Element: " << ELEMENT(myHetAtom);
+                      std::cout << "; Charge: " << CHARGE(myHetAtom);
+                      std::cout << std::endl;
+                    }
+                    catch (const std::exception& e) {
+                      std::cout << e.what() << std::endl;
+                    }
+                  }
+                  catch (const std::exception& e) {
+                    std::cout << e.what() << std::endl;
+                  }
+                }
+                catch (const std::exception& e) {
+                  std::cout << e.what() << std::endl;
+                }
+              }
+              catch (const std::exception& e) {
+                std::cout << e.what() << std::endl;
+              }
+            }
+          }
+          catch (const std::exception& e) {
+            std::cout << e.what() << std::endl;
+          }
+        }
+        catch (const std::exception& e) {
+          std::cout << e.what() << std::endl;
+        }
       }
+      catch (const std::exception& e) {
+        std::cout << e.what() << std::endl;
+      }
+
+      std::cout << "Model width: " << MODEL_WIDTH(myModel) << std::endl;
+      std::cout << "Model diameter: " << MODEL_DIAMETER(myModel) << std::endl;
+      std::cout << std::endl;
+
+      auto [surface, buried] = NUMBER_OF_SURFACE_AND_BURIED_RESIDUES(myModel);
+      std::cout << "Residues in model: " << NUMBER_OF_RESIDUES(myModel) << std::endl;
+      std::cout << "Surface residues: " << surface << "; Buried residues: " << buried << std::endl;
+      std::cout << std::endl;
+
+      auto [surfaceStats, buriedStats] = GET_SURFACE_AND_BURIED_STATS(myModel);
+      std::cout << "Statistics for surface residues:" << std::endl;
+      for (auto&& [name, count] : surfaceStats) {
+        std::cout << name << " " << count << std::endl;
+      }
+      std::cout << "Statistics for buried residues:" << std::endl;
+      for (auto&& [name, count] : buriedStats) {
+        std::cout << name << " " << count << std::endl;
+      }
+
+      auto [polarSurfacePortion, polarBuriedPortion] = GET_PORTION_OF_POLAR_SURFACE_AND_BURIED(myModel);
+      std::cout << "Portion of polar residues on the surface: " << polarSurfacePortion;
+      std::cout << "; Portion of polar residues in the core: " << polarBuriedPortion;
+      std::cout << std::endl;
+      std::cout << std::endl;
     }
-
-    HET_ATOM myHetAtom = FIND_ATOM(myHetResidue, "4438");
-    auto[hetX, hetY, hetZ] = COORDINATES(myHetAtom);
-    std::cout << "      Found atom " << ATOM_NAME(myHetAtom) << " " << ID(myHetAtom);
-    std::cout << "; (" << hetX << ", " << hetY << ", " << hetZ << ")";
-    std::cout << "; Element: " << ELEMENT(myHetAtom);
-    std::cout << "; Charge: " << CHARGE(myHetAtom);
-    std::cout << std::endl;
-
-    std::cout << "Model width: " << MODEL_WIDTH(myModel) << std::endl;
-    std::cout << "Model diameter: " << MODEL_DIAMETER(myModel) << std::endl;
-    std::cout << std::endl;
-
-    auto [surface, buried] = NUMBER_OF_SURFACE_AND_BURIED_RESIDUES(myModel);
-    std::cout << "Residues in model: " << NUMBER_OF_RESIDUES(myModel) << std::endl;
-    std::cout << "Surface residues: " << surface << "; Buried residues: " << buried << std::endl;
-    std::cout << std::endl;
-
-    auto [surfaceStats, buriedStats] = GET_SURFACE_AND_BURIED_STATS(myModel);
-    std::cout << "Statistics for surface residues:" << std::endl;
-    for (auto&& [name, count] : surfaceStats) {
-      std::cout << name << " " << count << std::endl;
+    catch (const std::exception& e) {
+      std::cout << e.what() << std::endl;
     }
-    std::cout << "Statistics for buried residues:" << std::endl;
-    for (auto&& [name, count] : buriedStats) {
-      std::cout << name << " " << count << std::endl;
-    }
-
-    auto [polarSurfacePortion, polarBuriedPortion] = GET_PORTION_OF_POLAR_SURFACE_AND_BURIED(myModel);
-    std::cout << "Portion of polar residues on the surface: " << polarSurfacePortion;
-    std::cout << "; Portion of polar residues in the core: " << polarBuriedPortion;
-    std::cout << std::endl;
-    std::cout << std::endl;
   }
 
 private:
@@ -263,7 +351,18 @@ public:
   TestClustal(const std::string& file) : myClustal_(CLUSTAL(file)) {}
 
   void performTest() {
+    std::size_t index, n;
+
+    std::cout << std::endl;
+    std::cout << "########################################################" << std::endl;
+    std::cout << std::endl;
     std::cout << "TEST: Clustal" << std::endl;
+    std::cout << "Type index of a column in your MSA:" << std::endl;
+    std::cin >> index;
+    std::cout << "Type number of best scoring columns to be printed:" << std::endl;
+    std::cin >> n;
+    std::cout << std::endl;
+
     for (std::size_t i = 0; i < NUMBER_OF_SEQUENCES(myClustal_); ++i) {
       std::cout << ID(myClustal_, i) << ": " << SEQUENCE(myClustal_, i) << std::endl;
     }
@@ -282,12 +381,10 @@ public:
     }
     std::cout << std::endl;
 
-    std::size_t index = 97;
     std::cout << "Sum of pairs of column at index " << index << ": " << (int) SUM_OF_PAIRS(myClustal_, blosum64::triangularMatrix, blosum64::order, index) << std::endl;
     std::cout << "Sum of pairs with triangular matrix: " << (int) SUM_OF_PAIRS(myClustal_, blosum64::triangularMatrix, blosum64::order) << std::endl;
     std::cout << std::endl;
 
-    std::size_t n = 10;
     std::cout << n << " best scoring columns:" << std::endl;
     for (auto&& [score, index, column] : BEST_SCORING_COLUMNS(myClustal_, blosum64::triangularMatrix, blosum64::order, n)) {
       std::cout << "Score: " << (int) score << "; Index: " << index;
